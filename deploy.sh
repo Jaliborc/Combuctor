0b6e1b9dcf4d14d9d2bcf8a259b74b616130fbbd
Tuller <j85ason+git@gmail.com>
2010-10-14 22:26:15 -0400

    4.0.1 bugfixes

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-rm -rf "${WOW_BETA_ADDON_DIR}Combuctor"
-rm -rf "${WOW_BETA_ADDON_DIR}Combuctor_Config"
-rm -rf "${WOW_BETA_ADDON_DIR}Combuctor_Sets"
+rm -rf "${WOW_ADDON_DIR}Combuctor"
+rm -rf "${WOW_ADDON_DIR}Combuctor_Config"
+rm -rf "${WOW_ADDON_DIR}Combuctor_Sets"
 
-cp -r Combuctor "${WOW_BETA_ADDON_DIR}"
-cp -r Combuctor_Config "${WOW_BETA_ADDON_DIR}"
-cp -r Combuctor_Sets "${WOW_BETA_ADDON_DIR}"
+cp -r Combuctor "${WOW_ADDON_DIR}"
+cp -r Combuctor_Config "${WOW_ADDON_DIR}"
+cp -r Combuctor_Sets "${WOW_ADDON_DIR}"
 
-cp LICENSE "${WOW_BETA_ADDON_DIR}Combuctor"
-cp LICENSE "${WOW_BETA_ADDON_DIR}Combuctor_Config"
-cp LICENSE "${WOW_BETA_ADDON_DIR}Combuctor_Sets"
+cp LICENSE "${WOW_ADDON_DIR}Combuctor"
+cp LICENSE "${WOW_ADDON_DIR}Combuctor_Config"
+cp LICENSE "${WOW_ADDON_DIR}Combuctor_Sets"
 
-cp README.textile  "${WOW_BETA_ADDON_DIR}Combuctor"
+cp README.textile  "${WOW_ADDON_DIR}Combuctor"
